dddc097cc5b507f724b2a510a983c6c79d5cc1e2
KHRDilshan <ravin@silverlineit.co>
2024-10-23 22:20:23 +0530

    r

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ db.connect((err) => {
         console.error('Error connecting to the database:', err);
         return;
     }
-    console.log('Connected to MySQL databasee.');
+    console.log('Connected to MySQL databas.');
 });
 
 module.exports = db;
